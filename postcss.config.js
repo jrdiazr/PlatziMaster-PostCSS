2fdad3b46447acd4f1f74ade47fd5fcb60fafbb7
Jose Roberto Diaz Rodriguez <diazrodriguez.jr@gmail.com>
2020-04-11 19:25:08 -0500

    Final del curso con retos

---
+++ postcss.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	plugins: [
+		// require('autoprefixer')({
+		//   grid: true
+		// }),
+		require("postcss-import")({
+			plugins: [require("stylelint")],
+		}),
+		require("postcss-font-magician")({
+			variants: {
+				Lato: {
+					"300": [],
+					"400": [],
+				},
+			},
+		}),
+		require("postcss-cssnext")({
+			features: {
+				autoprefixer: {
+					grid: true,
+					flexbox: false,
+				},
+				customProperties: false,
+				calc: false,
+			},
+		}),
+		require("css-mqpacker"),
+		require("cssnano"),
+	],
+};
